7af412eb2cab99a36a296a88d0d92604fe25b042
Brendan Jackman <bhenryj0117@gmail.com>
2021-01-30 17:04:17 +0100

    Add logic to fetch and export Awair score

--- main.go
+++ main.go
@@ -8,12 +8,49 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
 	awairAddress = flag.String("awair-address", "", "Base URL of Awair local API server")
 )
+
+// The next few bits are more or less copied from
+// https://godoc.org/github.com/prometheus/client_golang/prometheus#example-Collector
+
+var (
+	awairScoreDesc = prometheus.NewDesc("awair_score", "Awair's 'Score' metric", nil, nil)
+)
+
+type Collector struct {
+	awairBaseURL string
+}
+
+func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
+	// This bit is bad, it assumes that Collect will never fail to fetch
+	// metrics. If it does fail at inopportune times, we violate the
+	// contract of the Collector interface and things will presumably get
+	// messed up in confusing ways.
+	prometheus.DescribeByCollect(c, ch)
+}
+
+func (c *Collector) Collect(ch chan<- prometheus.Metric) {
+	data, err := Get(c.awairBaseURL)
+	if err != nil {
+		// TODO: Count these errors. We need metrics for our metrics.
+		log.Printf("Error getting data from Awair: %v", err)
+		return
+	}
+
+	m, err := prometheus.NewConstMetric(awairScoreDesc, prometheus.GaugeValue, float64(data.Score))
+	if err != nil {
+		// TODO: Count these errors. We need metrics for our metrics.
+		log.Printf("NewConstMetric: %v", err)
+		return
+	}
+	ch <- m
+}
 
 // AwairAirDataResponse is the structure we expect from the /air/data-latest
 // endpoints of the Awair local API.
@@ -34,29 +71,40 @@ type AirDataResponse struct {
 	Pm10Est       int64     `json:"pm10_est"`
 }
 
+func Get(baseURL string) (*AirDataResponse, error) {
+	url := baseURL + "/air-data/latest"
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to GET from Awair at %q: %v", url, err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+
+	if resp.StatusCode > 200 || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("Awair returned an error: %v", http.StatusText(resp.StatusCode))
+	}
+
+	data := AirDataResponse{}
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode JSON: %v", err)
+	}
+
+	return &data, nil
+}
+
 func main() {
 	flag.Parse()
 	if len(*awairAddress) == 0 {
 		log.Fatalf("--awair-address must be provided (%q)", *awairAddress)
 	}
 
+	err := prometheus.Register(&Collector{awairBaseURL: *awairAddress})
+	if err != nil {
+		log.Fatalf("Failed to register Prometheus collector: %v", err)
+	}
+
 	http.Handle("/metrics", promhttp.Handler())
-
-	url := *awairAddress + "/air-data/latest"
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatalf("Failed to GET %q: %v", url, err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("Response status:", resp.Status)
-
-	data := AirDataResponse{}
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
-		log.Fatalf("Failed to decode JSON: %v", err)
-	}
-	log.Printf("%+v", data)
-
 	http.ListenAndServe(":8080", nil)
 }
